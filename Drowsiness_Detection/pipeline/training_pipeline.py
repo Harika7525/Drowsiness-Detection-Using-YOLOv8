57b6d6d1ac3d377f6722832d43e26b87634d0082
swapnil sonawane <sswapnil0098@gmail.com>
2023-10-20 16:35:33 +0530

    model trainer added

--- Drowsiness_Detection/pipeline/training_pipeline.py
+++ Drowsiness_Detection/pipeline/training_pipeline.py
@@ -4,14 +4,17 @@ from Drowsiness_Detection.logger import logging
 from Drowsiness_Detection.exception import AppException
 from Drowsiness_Detection.component.data_ingestion import DataIngestion
 from Drowsiness_Detection.component.data_validator import DataValidation
+from Drowsiness_Detection.component.model_trainer import ModelTrainer
 
 from Drowsiness_Detection.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
+    ModelTrainerConfig,
 )
 from Drowsiness_Detection.entity.artifacts_entity import (
     DataIngestionArtifact,
     DataValidationArtifact,
+    ModelTrainerArtifact,
 )
 
 
@@ -19,6 +22,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
     def start_Data_ingestion(self) -> DataIngestionArtifact:
         try:
@@ -59,12 +63,28 @@ class TrainPipeline:
         except Exception as e:
             raise AppException(e, sys) from e
 
+    def start_model_trainer(self) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
+
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_Data_ingestion()
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+
+            else:
+                raise Exception("Your data is not in correct format")
 
         except Exception as e:
             raise AppException(e, sys)
